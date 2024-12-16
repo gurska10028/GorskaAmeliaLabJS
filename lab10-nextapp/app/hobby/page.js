51966670a0dc2d033cd7b798b2fdf704da0300d6
Amelia Górska <119795055+gurska10028@users.noreply.github.com>
2024-12-16 11:36:07 +0100

    Delete lab10-nextapp directory

--- lab10-nextapp/app/hobby/page.js
+++
@@ -1,8 +0,0 @@
-export default function Hobby() {
-    return (
-        <main>
-            <h1>Moje zainteresowania</h1>
-            <p>Interesuję się programowaniem, żeglowaniem i podróżami. W szczególności fascynuje mnie rozwój aplikacji webowych i sztuczna inteligencja.</p>
-        </main>
-    );
-}
